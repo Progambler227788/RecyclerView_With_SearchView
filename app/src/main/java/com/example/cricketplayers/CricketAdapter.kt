cb22c8c954ec2dede406b9f634185b55f972dbe3
Talha <talha@gmail.com>
2023-07-14 13:02:20 +0500

    Expandable Added

--- app/src/main/java/com/example/cricketplayers/CricketAdapter.kt
+++ app/src/main/java/com/example/cricketplayers/CricketAdapter.kt
@@ -9,7 +9,11 @@ import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.cricketplayers.databinding.RvPlayersBinding
 
 class CricketAdapter (private var dataList : ArrayList<CricketData>,var context : Context) : RecyclerView.Adapter<CricketAdapter.MyViewHolder> (){
-   inner class MyViewHolder(var binding : RvPlayersBinding) : ViewHolder(binding.root)
+   inner class MyViewHolder(var binding : RvPlayersBinding) : ViewHolder(binding.root){
+      fun collapse(){
+         binding.playerDescription.visibility = View.GONE
+      }
+   }
 
    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
       val binding = RvPlayersBinding.inflate(LayoutInflater.from(context),parent,false)
@@ -31,12 +35,30 @@ class CricketAdapter (private var dataList : ArrayList<CricketData>,var context 
       currentBinding.playerName.text = dataList[position].name
       currentBinding.playerDescription.text = dataList[position].description
       val isExpandable = dataList[position].isExpandable
+
       currentBinding.playerDescription.visibility = if(isExpandable) View.VISIBLE else View.GONE
       currentBinding.constraintLayout.setOnClickListener {
+         isAnyItemExpanded(position)
          dataList[position].isExpandable = !dataList[position].isExpandable
          notifyItemChanged(position)
       }
 
 
+   }
+   fun isAnyItemExpanded(position: Int) {
+      val pos = dataList.indexOfFirst { it.isExpandable
+      }
+      if(pos>=0 && pos!=position){
+         dataList[pos].isExpandable = false
+         notifyItemChanged(pos,0)
+
+      }
+   }
+
+   override fun onBindViewHolder(holder: MyViewHolder, position: Int, payloads: MutableList<Any>) {
+      if(payloads.isNotEmpty() && payloads[0]==0)
+         holder.collapse()
+      else
+      super.onBindViewHolder(holder, position, payloads)
    }
 }
